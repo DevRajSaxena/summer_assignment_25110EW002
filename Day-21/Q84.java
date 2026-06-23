b59a0ab2f24c9668486edadddcf943ae85b1e651
DevRajSaxena <saxenadevraj9@gmail.com>
2026-06-23 21:55:37 +0530

    Add files via upload

---
+++ Day-21/Q84.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Q84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String upper = str.toUpperCase();
+        System.out.println("Uppercase String = " + upper);
+    }
+}
